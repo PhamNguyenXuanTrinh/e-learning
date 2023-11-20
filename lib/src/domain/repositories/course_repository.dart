8541fd7f1df63d9fadc6eb7197cb1e9830010cb8
Tien Truong <62138943+tientt1304@users.noreply.github.com>
2023-11-21 02:59:32 +0700

    feat: add course repository and implement

---
+++ lib/src/domain/repositories/course_repository.dart
@@ -0,0 +1,7 @@
+import 'package:elearning/src/domain/models/course_model.dart';
+
+import '../../core/resources/data_state.dart';
+
+abstract class CourseRepository {
+  Future<DataState<List<CourseModel>?>> getCourses();
+}
