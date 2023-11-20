98828d2989b50a8d7c5ba4707d13a1f4448add9b
Anh Thư Đặng <anhthudang16022002@gmail.com>
2023-11-20 16:24:34 +0700

    feat: create home screen
    (cherry picked from commit 7be7a9606f3bf7cfcb024d21106b4a865c26b5ef)

--- lib/src/presentation/views/home/home_screen.dart
+++ lib/src/presentation/views/home/home_screen.dart
@@ -1,71 +1,67 @@
-import 'package:elearning/src/injector/injector.dart';
-import 'package:elearning/src/presentation/bloc/home_bloc/home_bloc.dart';
+import 'package:elearning/src/core/utils/constants/strings.dart';
+import 'package:elearning/src/domain/models/home_screen/home_model.dart';
 import 'package:elearning/src/presentation/views/home/widgets/avatar_widget.dart';
 import 'package:elearning/src/presentation/views/home/widgets/img_list_widget.dart';
 import 'package:elearning/src/presentation/views/home/widgets/learned_widget.dart';
 import 'package:elearning/src/presentation/views/home/widgets/learning_plan_widget.dart';
 import 'package:elearning/src/presentation/views/home/widgets/meet_up_widget.dart';
 import 'package:elearning/src/presentation/views/home/widgets/title_widget.dart';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key});
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => getIt<HomeBloc>()..add(HomeStarted()),
-      child: BlocBuilder<HomeBloc, HomeState>(
-        builder: (context, state) {
-          if (state is HomeLoadSuccess) {
-            final homeData = state.homeData;
+    // HomeModel với các giá trị tương ứng
+    HomeModel homeModel = HomeModel(
+      learnText: AppStrings.learn,
+      myCourseText: AppStrings.mycourse,
+      time46Text: AppStrings.time46,
+      time60Text: AppStrings.time60,
+      learning: AppStrings.learning,
+      packDesign: AppStrings.packDesign,
+      proDesign: AppStrings.proDesign,
+      learn40: AppStrings.learn40,
+      learn48: AppStrings.learn48,
+      learn6: AppStrings.learn6,
+      learn24: AppStrings.learn24,
+    );
 
-            return Scaffold(
-              body: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Stack(
-                      children: [
-                        Container(
-                          height: 180,
-                          margin: const EdgeInsets.only(bottom: 70),
-                          color: Theme.of(context).primaryColor,
-                          padding: const EdgeInsets.all(20.0),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              TitleWidget(homeData: homeData),
-                              Spacer(),
-                              AvatarWidget(homeData: homeData),
-                            ],
-                          ),
-                        ),
-                        Positioned(
-                          bottom: 20,
-                          left: 10,
-                          right: 10,
-                          child: LearnedWidget(homeData: homeData),
-                        ),
-                      ],
-                    ),
-                    ImgListWidget(
-                      onPressed: () {},
-                      homeData:homeData
-                    ),
-                    LearningPlanWidget(
-                      homeData: homeData,
-                    ),
-                    const MeetupWidget(),
-                  ],
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  height: 180,
+                  margin: const EdgeInsets.only(bottom: 70),
+                  color: Colors.blue,
+                  padding: const EdgeInsets.all(20.0),
+                  child: const Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TitleWidget(),
+                      Spacer(),
+                      AvatarWidget(),
+                    ],
+                  ),
                 ),
-              ),
-            );
-          } else {
-            return CircularProgressIndicator();
-          }
-        },
+                Positioned(
+                    bottom: 20,
+                    left: 10,
+                    right: 10,
+                    child: LearnedWidget(homeModel: homeModel)),
+              ],
+            ),
+            ImgListWidget(
+              onPressed: () {},
+            ),
+            LearningPlanWidget(homeModel: homeModel),
+            const MeetupWidget(),
+          ],
+        ),
       ),
     );
   }
--- lib/src/presentation/views/home/widgets/learning_plan_widget.dart
+++ lib/src/presentation/views/home/widgets/learning_plan_widget.dart
@@ -1,18 +1,23 @@
+import 'package:elearning/src/core/utils/constants/imgs.dart';
 import 'package:flutter/material.dart';
-import 'package:elearning/src/core/utils/constants/strings.dart';
-import '../../../../domain/models/home_model.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:elearning/src/domain/models/home_screen/home_model.dart';
+
 
 class LearningPlanWidget extends StatefulWidget {
-  final HomeModel? homeData;
+  final HomeModel homeModel;
 
-  const LearningPlanWidget({super.key, required this.homeData});
+  const LearningPlanWidget({
+    super.key,
+    required this.homeModel,
+  });
 
   @override
   _LearningPlanWidgetState createState() => _LearningPlanWidgetState();
 }
 
 class _LearningPlanWidgetState extends State<LearningPlanWidget> {
-  bool isShowMore = false;
+  bool isMoRong = false;
 
   @override
   Widget build(BuildContext context) {
@@ -23,9 +28,9 @@ class _LearningPlanWidgetState extends State<LearningPlanWidget> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Text(
-            AppStrings.learning,
-            style: TextStyle(
+          Text(
+            widget.homeModel.learning,
+            style: const TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.bold,
             ),
@@ -35,13 +40,15 @@ class _LearningPlanWidgetState extends State<LearningPlanWidget> {
           ),
           Container(
             width: chieuRongManHinh - 25,
-            height: isShowMore ? 240 : 120,
+            height: isMoRong
+                ? 240
+                : 120, // Điều chỉnh chiều cao dựa trên trạng thái
             decoration: BoxDecoration(
-              color: Theme.of(context).scaffoldBackgroundColor,
+              color: Colors.white,
               borderRadius: BorderRadius.circular(10.0),
               boxShadow: [
                 BoxShadow(
-                  color: Theme.of(context).shadowColor.withOpacity(0.2),
+                  color: Colors.grey.withOpacity(0.5),
                   spreadRadius: 2,
                   blurRadius: 5,
                   offset: const Offset(0, 2),
@@ -59,42 +66,12 @@ class _LearningPlanWidgetState extends State<LearningPlanWidget> {
                       children: [
                         Column(
                           children: [
-                            SizedBox(
-                              height: 20,
-                              width: 20,
-                              child: CircularProgressIndicator(
-                                value: (widget.homeData?.learningPlan[0]
-                                            .studiedLessons ??
-                                        0) /
-                                    (widget.homeData?.learningPlan[0]
-                                                .totalLessons ??
-                                            1)
-                                        .toDouble(),
-                                backgroundColor:
-                                    Theme.of(context).secondaryHeaderColor,
-                                valueColor: AlwaysStoppedAnimation<Color>(
-                                  Theme.of(context).cardColor,
-                                ),
-                              ),
+                            SvgPicture.asset(
+                              AppImages.imglearn2,
                             ),
                             const SizedBox(height: 15),
-                            SizedBox(
-                              height: 20,
-                              width: 20,
-                              child: CircularProgressIndicator(
-                                value: (widget.homeData?.learningPlan[1]
-                                            .studiedLessons ??
-                                        0) /
-                                    (widget.homeData?.learningPlan[1]
-                                                .totalLessons ??
-                                            1)
-                                        .toDouble(),
-                                backgroundColor:
-                                    Theme.of(context).secondaryHeaderColor,
-                                valueColor: AlwaysStoppedAnimation<Color>(
-                                  Theme.of(context).cardColor,
-                                ),
-                              ),
+                            SvgPicture.asset(
+                              AppImages.imglearn1,
                             ),
                           ],
                         ),
@@ -104,16 +81,14 @@ class _LearningPlanWidgetState extends State<LearningPlanWidget> {
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(
-                                widget.homeData?.learningPlan[0].courseName ??
-                                    '',
+                                widget.homeModel.packDesign,
                                 style: const TextStyle(
                                   fontSize: 16,
                                 ),
                               ),
                               const SizedBox(height: 15),
                               Text(
-                                widget.homeData?.learningPlan[1].courseName ??
-                                    '',
+                                widget.homeModel.proDesign,
                                 style: const TextStyle(
                                   fontSize: 16,
                                 ),
@@ -121,30 +96,23 @@ class _LearningPlanWidgetState extends State<LearningPlanWidget> {
                             ],
                           ),
                         ),
-                        const SizedBox(width: 8.0),
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.end,
                           children: [
                             Row(
                               children: [
                                 Text(
-                                  '${widget.homeData?.learningPlan[0].studiedLessons ?? 0}',
+                                  widget.homeModel.learn40,
                                   style: const TextStyle(
                                     fontSize: 16,
+                                    color: Colors.black,
                                   ),
                                 ),
                                 Text(
-                                  AppStrings.slash,
-                                  style: TextStyle(
+                                  widget.homeModel.learn48,
+                                  style: const TextStyle(
                                     fontSize: 16,
-                                    color: Theme.of(context).cardColor,
-                                  ),
-                                ),
-                                Text(
-                                  '${widget.homeData?.learningPlan[0].totalLessons ?? 0}',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Theme.of(context).cardColor,
+                                    color: Colors.grey,
                                   ),
                                 ),
                               ],
@@ -153,23 +121,17 @@ class _LearningPlanWidgetState extends State<LearningPlanWidget> {
                             Row(
                               children: [
                                 Text(
-                                  '${widget.homeData?.learningPlan[1].studiedLessons ?? 0}',
+                                  widget.homeModel.learn6,
                                   style: const TextStyle(
                                     fontSize: 16,
+                                    color: Colors.black,
                                   ),
                                 ),
                                 Text(
-                                  AppStrings.slash,
-                                  style: TextStyle(
+                                  widget.homeModel.learn24,
+                                  style: const TextStyle(
                                     fontSize: 16,
-                                    color: Theme.of(context).cardColor,
-                                  ),
-                                ),
-                                Text(
-                                  '${widget.homeData?.learningPlan[1].totalLessons ?? 0}',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Theme.of(context).cardColor,
+                                    color: Colors.grey,
                                   ),
                                 ),
                               ],
@@ -179,17 +141,18 @@ class _LearningPlanWidgetState extends State<LearningPlanWidget> {
                       ],
                     ),
                     const SizedBox(height: 8.0),
+                    // Đặt nút "Xem thêm" ở giữa
                     Expanded(
                       child: Align(
                         alignment: Alignment.bottomCenter,
                         child: TextButton(
                           onPressed: () {
                             setState(() {
-                              isShowMore = !isShowMore;
+                              isMoRong = !isMoRong;
                             });
                           },
                           child: Text(
-                            isShowMore ? AppStrings.less : AppStrings.more,
+                            isMoRong ? "Thu gọn" : "Xem thêm",
                           ),
                         ),
                       ),
